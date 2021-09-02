9ca3f353e19e3004c8904e542468c9bea70738ae
github-action-benchmark <github@users.noreply.github.com>
2021-09-02 17:00:48 +0000

    add Benchmark (cargo) benchmark result for a6083522ec312af1a607ee39af08611373d76ff6

---
+++ docs/bench/main/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1630602048129,
+  "repoUrl": "https://github.com/jmfiaschi/json_value_remove",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "distinct": true,
+          "id": "a6083522ec312af1a607ee39af08611373d76ff6",
+          "message": "fix(project): benches",
+          "timestamp": "2021-09-02T18:52:42+02:00",
+          "tree_id": "79de692e6a05bf0929622791b0f3d6482cf75216",
+          "url": "https://github.com/jmfiaschi/json_value_remove/commit/a6083522ec312af1a607ee39af08611373d76ff6"
+        },
+        "date": 1630602047364,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Remove/remove_in_array",
+            "value": 733,
+            "range": "± 34",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Remove/remove_in_object",
+            "value": 557,
+            "range": "± 27",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
