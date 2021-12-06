d315faaaf79663ff4da95b5b52a0dc3f758733fb
github-action-benchmark <github@users.noreply.github.com>
2021-12-06 22:40:09 +0000

    add Benchmark (cargo) benchmark result for 1ee4cd06ef89609cfee005cf7fbbcd88e24c42c1

---
+++ docs/bench/beta/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1638830409267,
+  "repoUrl": "https://github.com/jmfiaschi/json_value_remove",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "distinct": true,
+          "id": "1ee4cd06ef89609cfee005cf7fbbcd88e24c42c1",
+          "message": "fix(Cargo): typo for LICENSES",
+          "timestamp": "2021-12-06T23:34:13+01:00",
+          "tree_id": "2bbc9e024fc9384440203dd440db633e54c972ca",
+          "url": "https://github.com/jmfiaschi/json_value_remove/commit/1ee4cd06ef89609cfee005cf7fbbcd88e24c42c1"
+        },
+        "date": 1638830408423,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Remove/remove_in_array",
+            "value": 552,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Remove/remove_in_object",
+            "value": 408,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
