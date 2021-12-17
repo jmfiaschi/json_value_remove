8f1e570020dcd5aa2b8a70be4920533a92fbffdb
github-action-benchmark <github@users.noreply.github.com>
2021-12-17 21:55:52 +0000

    add Benchmark (cargo) benchmark result for 56f3925a6c57b7f376a75b10c6a0bba3846f872d

--- docs/bench/beta/data.js
+++ docs/bench/beta/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638830409267,
+  "lastUpdate": 1639778152623,
   "repoUrl": "https://github.com/jmfiaschi/json_value_remove",
   "entries": {
     "Benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "Remove/remove_in_object",
             "value": 408,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "committer": {
+            "email": "jm.fiaschi@gmail.com",
+            "name": "jmfiaschi",
+            "username": "jmfiaschi"
+          },
+          "distinct": true,
+          "id": "56f3925a6c57b7f376a75b10c6a0bba3846f872d",
+          "message": "fix(Cargo): update version automatically",
+          "timestamp": "2021-12-17T22:49:01+01:00",
+          "tree_id": "040cd61dacb2a8b903e43f3b92002a6359148d54",
+          "url": "https://github.com/jmfiaschi/json_value_remove/commit/56f3925a6c57b7f376a75b10c6a0bba3846f872d"
+        },
+        "date": 1639778151774,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Remove/remove_in_array",
+            "value": 664,
+            "range": "± 29",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Remove/remove_in_object",
+            "value": 510,
+            "range": "± 31",
             "unit": "ns/iter"
           }
         ]
